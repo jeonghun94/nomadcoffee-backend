0822c99574e00815f8db64858873f8bdf6a00a01
조정훈 <jeongh1021@naver.com>
2022-03-29 14:42:29 +0900

    typDefs, resolvers 분할

--- server.js
+++ server.js
@@ -1,54 +1,11 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
-import { ApolloServer, gql } from "apollo-server";
-
-const client = new PrismaClient();
-
-//  GraphQL schema 모든 기본 선언은 선택적 필수값으로 표현 하려면 ! 표시
-const typeDefs = gql`
-  type Movie {
-    id: Int!
-    title: String!
-    year: Int!
-    genre: String
-    creadeAt: String!
-    updatedAt: String!
-  }
-
-  type Query {
-    movies: [Movie]
-    movie(id: Int!): Movie
-  }
-
-  type Mutation {
-    createMovie(title: String!, year: Int!, genre: String): Movie
-    deleteMovie(id: Int!): Movie
-    updateMovie(id: Int!, year: Int!): Movie
-  }
-`;
-
-const resolvers = {
-  Query: {
-    movies: () => client.movie.findMany(),
-    movie: (_, { id }) => client.movie.findUnique({ where: { id } }),
-  },
-  Mutation: {
-    createMovie: (_, { title, year, genre }) =>
-      client.movie.create({
-        data: {
-          title,
-          year,
-          genre,
-        },
-      }),
-    deleteMovie: (_, { id }) => client.movie.delete({ where: { id } }),
-    updateMovie: (_, { id, year }) =>
-      client.movie.update({ where: { id }, data: { year } }),
-  },
-};
+import { ApolloServer } from "apollo-server";
+import schema from "./schema";
 
 const PORT = process.env.PORT;
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  schema,
+});
 
 server.listen(PORT).then(() => {
   console.log(`🚀  Server ready at ${PORT}`);
