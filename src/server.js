67129519be5548316a353fc5fd33152eb24c2e1c
Jeong Hun <66016555+jeonghun94@users.noreply.github.com>
2022-04-07 10:17:14 +0900

    instrospection add

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ const startServer = async () => {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
+    introspection: true,
     context: async ({ req }) => {
       return {
         loggedInUser: await getUser(req.headers.token),
